03fbf0e023d4033771158d1cd82bb1bd8969ccf6
chwangmin <ckm7907cb@gmail.com>
2024-05-15 22:29:48 +0900

    feat : spring security filter 설정 - 스프링 필터들, 로그 확인 - jwt를 사용하여 로그인 직접구현하여 필요없는 필터들 disable - requestMatcher로 특정 path 만 filter chain 진입하도록 설정 - security filter chain ignore + /user/**로 설정

--- backend/src/main/java/com/ssafy/home/global/config/security/WebSecurityConfiguration.java
+++ backend/src/main/java/com/ssafy/home/global/config/security/WebSecurityConfiguration.java
@@ -5,22 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableWebSecurity(debug = true)
 public class WebSecurityConfiguration {
 
     final private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatchers((auth) -> auth.requestMatchers("/test/**"));
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((session) -> session
@@ -35,8 +37,16 @@ public class WebSecurityConfiguration {
                 .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
 
                 .httpBasic(withDefaults());
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
 
         return http.build();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+
+        return web -> web.ignoring().requestMatchers("/user/**");
+    }
 }
